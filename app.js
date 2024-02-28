e02383e946a0e85e2658e56f766214d787f09757
vinaykumar-coder <vinaykumar12264@gmail.com>
2024-02-28 20:20:35 +0530

    leaned about jsx

--- app.js
+++ app.js
@@ -1,26 +1,10 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
+import { ReactDOM, createRoot } from "react-dom/client";
 
+// JSX - HTML -like or XML -like syntax.
+// JSX (transpiled before it reaces the JS engine) - Parcel - Babel
+// JSX => React.createElement => ReactElement-JS oject => HTMLElement(render).
+const jsxHeading = <h1 className="head">Namaste react using JSX</h1>;
 
-// ReactElement(object) => HTML (browser understands);
-
-const parent = React.createElement(
-  "div",
-  { id: "parent" },
-  [
-    React.createElement(
-        "div",
-        { id: "child" },
-        [React.createElement("h1", {}, "This is Namaste React."),React.createElement("h2", {}, "I'm an h2 element")]
-      ),
-      React.createElement(
-        "div",
-        { id: "child2" },
-        [React.createElement("h1", {}, "I'm an h1 tag"),React.createElement("h2", {}, "I'm an h2 element")]
-      )
-  ]
-);
-
-// const heading = React.createElement("h1", {id: "heading"}, "Hello World! from React....");
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent);
+const root = createRoot(document.getElementById("root"));
+root.render(jsxHeading);
