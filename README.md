a629f036817e2a32aaf005b0f1ce140fe578eb28
vinaykumar-coder <vinaykumar12264@gmail.com>
2024-02-28 18:43:35 +0530

    added node modules and git ignore

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # Namaste react repository.
 # this will updates all the new tings i have done.
+
+# Parcel
+-dev build
+- local server
+- HMR -  hot module replacement
+- file watching algorithm. written in c++.
+- caching - faster builds.
+- Image optimization.
+- Minification 
+- bundling.
+- compress the files
+- consistent hashing.
+- code splitting
+- differecial bundling - to support older browsers.
+- diagnostic
+- error handling
+- tree shaking - will remove unused code
--- app.js
+++ app.js
@@ -1,3 +1,7 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+
 // ReactElement(object) => HTML (browser understands);
 
 const parent = React.createElement(
@@ -7,7 +11,7 @@ const parent = React.createElement(
     React.createElement(
         "div",
         { id: "child" },
-        [React.createElement("h1", {}, "I'm an h1 tag"),React.createElement("h2", {}, "I'm an h2 element")]
+        [React.createElement("h1", {}, "This is Namaste React."),React.createElement("h2", {}, "I'm an h2 element")]
       ),
       React.createElement(
         "div",
