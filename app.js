52da10bf4f02769b42f265d050c09be2fa000e19
vinaykumar-coder <vinaykumar12264@gmail.com>
2024-02-29 19:42:32 +0530

    Learned about React components

--- app.js
+++ app.js
@@ -1,10 +1,20 @@
 import React from "react";
 import { ReactDOM, createRoot } from "react-dom/client";
 
-// JSX - HTML -like or XML -like syntax.
-// JSX (transpiled before it reaces the JS engine) - Parcel - Babel
-// JSX => React.createElement => ReactElement-JS oject => HTMLElement(render).
-const jsxHeading = <h1 className="head">Namaste react using JSX</h1>;
+const Title = () =>  ( <h1 className="head">
+    Namaste react using JSX
+    </h1>);
+
+// React functional component.
+
+
+const HeadingComponent = () => (
+  <div>
+    {Title()}
+    <h1 className="heading">Namaste React functional component.</h1>
+  </div>
+);
 
 const root = createRoot(document.getElementById("root"));
-root.render(jsxHeading);
+// root.render(heading);
+root.render(<HeadingComponent />);
