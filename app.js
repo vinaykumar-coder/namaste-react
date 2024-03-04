103f5e71862bd4b98649e06dea99f531d9abba64
vinaykumar-coder <vinaykumar12264@gmail.com>
2024-03-04 20:01:24 +0530

    created the over view of the app

--- app.js
+++ app.js
@@ -1,20 +1,71 @@
 import React from "react";
 import { ReactDOM, createRoot } from "react-dom/client";
 
-const Title = () =>  ( <h1 className="head">
-    Namaste react using JSX
-    </h1>);
+/*
+- header 
+  -logo
+  -nav items
+-body
+  -search
+  -resturant container
+    -restro cards
+      - img
+      -name of the res,star rating,cuisine,delivery time
+- footer
+  -@copyright
+  -name
+  -address
+  -contact
+*/
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="logo-container">
+        <img
+          className="logo"
+          src="https://www.logodesign.net/logo/smoking-burger-with-lettuce-3624ld.png"
+        />
+      </div>
+      <div className="nav-items">
+        <ul>
+          <li>Home</li>
+          <li>About US</li>
+          <li>Contact Us</li>
+          <li>Cart</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
 
-// React functional component.
+const RestaurantCard = () => {
+  return (
+    <div className="res-card" style={{ background: "#f0f0f0"}}>
+      <img className="res-logo" alt="res-logo" src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/a6dd67e831e4819214055b0840f2c4fe"/>
+      <h3>Paradise biryani</h3>
+    </div>
+  );
+};
 
+const Body = () => {
+  return (
+    <div className="body">
+      <div className="search">Search</div>
+      <div className="restro-container">
+        <RestaurantCard />
+      </div>
+    </div>
+  );
+};
 
-const HeadingComponent = () => (
-  <div>
-    {Title()}
-    <h1 className="heading">Namaste React functional component.</h1>
-  </div>
-);
+const AppLayout = () => {
+  return (
+    <div className="app">
+      <Header />
+      <Body />
+    </div>
+  );
+};
 
 const root = createRoot(document.getElementById("root"));
-// root.render(heading);
-root.render(<HeadingComponent />);
+root.render(<AppLayout />);
