e66ab171cf98e822b31f51f8ef8c081552d0493b
vinaykumar-coder <vinaykumar12264@gmail.com>
2024-03-05 19:34:02 +0530

    created cards

--- app.js
+++ app.js
@@ -40,9 +40,12 @@ const Header = () => {
 
 const RestaurantCard = () => {
   return (
-    <div className="res-card" style={{ background: "#f0f0f0"}}>
-      <img className="res-logo" alt="res-logo" src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/a6dd67e831e4819214055b0840f2c4fe"/>
-      <h3>Paradise biryani</h3>
+    <div className="res-card">
+      <img className="res-logo" alt="res_logo" src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/yyw8mur3rcev7jizibpd"/>
+      <h3>Vinay Foods</h3>
+      <h4>Biryani, North Indian, Asian</h4>
+      <h4>4.4 stars</h4>
+      <h4>38 minutes</h4>
     </div>
   );
 };
@@ -52,6 +55,19 @@ const Body = () => {
     <div className="body">
       <div className="search">Search</div>
       <div className="restro-container">
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
         <RestaurantCard />
       </div>
     </div>
